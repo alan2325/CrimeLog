790baa46f9265069631038953e4f864845501ef2
alan2325 <alalkumar06@gmail.com>
2024-11-30 11:34:18 +0530

    email

--- project/app/views.py
+++ project/app/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render,redirect
 from .models import *
 from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User,auth
 from django.contrib import messages
 import re
 from django.core.files.storage import default_storage
+from django.core.mail import send_mail
+from django.conf import settings
 # from django.contrib.auth.decorators import login_required
 # from django.db.models import Q
 
@@ -64,7 +67,8 @@ def user_reg(req):
         phonenumber=req.POST['phonenumber']
         location=req.POST['location']
         password=req.POST['password']
-         # Validate email
+         
+          
         try:
             validate_email(email)
         except ValidationError:
@@ -78,6 +82,12 @@ def user_reg(req):
         try:
             data=User.objects.create(username=name,Email=email,phonenumber=phonenumber,location=location,password=password)
             data.save()
+            # Validate email
+            subject = 'Registration details '
+            message = 'ur account uname {}  and password {}'.format(name,password)
+            from_email = settings.EMAIL_HOST_USER
+            recipient_list = [email]
+            send_mail(subject, message, from_email, recipient_list,fail_silently=False)
             return redirect(login)
         except:
             messages.warning(req, "Email Already Exits , Try Another Email.")
